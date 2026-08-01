d4f6c6512d78ca87649265dc4ce256ed4ae258f8
FlutschiCH <flutschich@gmail.com>
2026-08-01 20:55:27 +0200

    Use replaceState to fix intermittent redirect issues when accessing root path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import HowToPage from './components/how_to_page.tsx';
 import Dashboard, { formatPrice, calculateDateBounds, getWeekStart, getPrecisionForSymbol } from './components/dashboard.tsx';
 
 export { formatPrice, calculateDateBounds, getWeekStart, getPrecisionForSymbol };
 
 export default function App() {
-  if (window.location.pathname === '/auth' || window.location.pathname === '/') {
-    window.history.pushState({}, '', '/dashboard');
+  if (typeof window !== 'undefined' && (window.location.pathname === '/auth' || window.location.pathname === '/')) {
+    window.history.replaceState({}, '', '/dashboard');
   }
 
   return <Dashboard />;
 }
+
