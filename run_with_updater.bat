ada87316c5a267d52d21ff3ab8c45f8d6f4c0b70
FlutschiCH <flutschich@gmail.com>
2026-08-14 03:57:17 +0200

    chore: remove deprecated run_with_updater.bat in favor of run_autoupdate.bat

--- run_with_updater.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Trading Autoupdater
-cd /d "%~dp0"
-echo ==========================================
-echo       Starting Trading Autoupdater        
-echo ==========================================
-echo.
-python autoupdate.py
-pause
